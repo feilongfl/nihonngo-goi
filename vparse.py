d302422196494e53fafa65b51b056390f50e6417
feilong <feilongphone@gmail.com>
2020-08-09 06:26:07 +0800

    change output file name

--- vparse.py
+++ vparse.py
@@ -10,14 +10,16 @@ def work(audiopath):
     audiotype = 'mp3'
     sound = AudioSegment.from_file(audiopath, format=audiotype)
     (chunks_path, _) = os.path.splitext(audiopath)
-    if not os.path.exists(chunks_path):os.mkdir(chunks_path)
+    if not os.path.exists(chunks_path): os.mkdir(chunks_path)
+    name_prefix = audiopath.split('/')
+    
     # split
     chunks = split_on_silence(sound, min_silence_len=300, silence_thresh=-70)
     
     # print('parse start')
     for i in range(len(chunks)):
         new = chunks[i]
-        save_name = os.path.join( chunks_path, '%04d.%s'%(i,audiotype))
+        save_name = os.path.join( chunks_path, '%s-%s-%04d.%s'%(name_prefix[1], name_prefix[2], i, audiotype))
         new.export(save_name, format=audiotype)
         # print('%04d'%i,len(new))
     # print('parse done')
